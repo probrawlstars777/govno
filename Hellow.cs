54fd5456896937cfd4aaa9ebc18b6eaf49f28ae8
probrawlstars777 <118114067+probrawlstars777@users.noreply.github.com>
2022-11-13 16:23:56 +0300

    Add files via upload

---
+++ Hellow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hellow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("kakashka");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
